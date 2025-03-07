655dcfcde4bbccc108aa33c0e656aa9b4fe3b11c
buğra uysal <uysalbugra134@gmail.com>
2025-03-07 22:50:25 +0300

    Visualizations were made by combining month and year dates in the dataset: visualization of total sales by month and year, visualization of maximum sales of stores, and display of information about the dataset description, information, head()

--- rpr.py
+++ rpr.py
@@ -1,6 +1,8 @@
 import pyodbc  # pypyodbc yerine pyodbc kullan
 import pandas as pd
-
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 server='DESKTOP-MKSKA1L'
 database='salesDB'
 
@@ -13,12 +15,40 @@ conn = pyodbc.connect(
     f'Trusted_Connection=yes;'
 )
 
-quary='SELECT * FROM train'
+quary=' SELECT STORE, MAX(WEEKLY_SALES) AS MAXPRİCE FROM train GROUP BY STORE '
+DAATE='SELECT YEAR(date) AS YIL, MONTH(date) AS AY, SUM(WEEKLY_SALES) AS TOTAL_SALES FROM train GROUP BY YEAR(date), MONTH(date) ORDER BY YIL, AY ASC'
+
 
 aa=pd.read_sql(quary, conn)
+bb=pd.read_sql(DAATE, conn)
+
 
 conn.close()
+""" tarih sütünu  ay ve yılı birleştirme """
+bb['tarih']= bb["YIL"].astype(str) + '-' + bb["AY"].astype(str)
 
 print(aa.head())
 print(aa.describe())
+print(aa.info())
+print(aa)
+sns.barplot(x=aa["STORE"], y=aa["MAXPRİCE"])
+plt.grid(True)
+plt.xticks(rotation=90)
+plt.show()
 
+plt.figure(figsize=(10, 5))
+plt.plot(bb["tarih"], bb["TOTAL_SALES"])
+plt.grid(True)
+plt.xticks(rotation=90)
+plt.show()
+
+plt.bar(bb['YIL'], bb['TOTAL_SALES'] )
+plt.xticks(rotation=90)
+plt.title("years")
+plt.show()
+
+
+plt.bar(bb['AY'], bb['TOTAL_SALES'])
+plt.xticks(rotation=90)
+plt.title("mount")
+plt.show()
